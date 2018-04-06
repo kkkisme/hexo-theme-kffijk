26b7c6a2655c070330178493c228c9ce8e167a08
huming <ming1.hu@meicloud.com>
2018-04-07 00:21:19 +0800

    initial

---
+++ source/js/script.js
@@ -0,0 +1,11 @@
+;(function() {
+  let btn = document.querySelector('#mobile-nav-toggle')
+  btn.addEventListener('click', function(e) {
+    e.stopPropagation()
+    document.body.classList.toggle('mobile-nav-on')
+  })
+
+  document.body.addEventListener('click', function(e) {
+    document.body.classList.remove('mobile-nav-on')
+  })
+})()
