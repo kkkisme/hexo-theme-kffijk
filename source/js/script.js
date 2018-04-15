c763387e17257c314893de156d9b32ee18202fa6
huming <ming1.hu@meicloud.com>
2018-04-15 22:00:49 +0800

    update

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,10 @@
-;(function() {
-  let btn = document.querySelector('#mobile-nav-toggle')
-  btn.addEventListener('click', function(e) {
-    e.stopPropagation()
-    document.body.classList.toggle('mobile-nav-on')
-  })
-
-  document.body.addEventListener('click', function(e) {
-    document.body.classList.remove('mobile-nav-on')
-  })
-})()
+(function() {
+	let btn = document.querySelector("#mobile-nav-toggle");
+	btn.addEventListener("click", function(e) {
+		e.stopPropagation();
+		document.body.classList.toggle("mobile-nav-on");
+	});
+	document.body.addEventListener("click", function(e) {
+		document.body.classList.remove("mobile-nav-on");
+	});
+})();
